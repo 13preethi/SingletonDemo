af3c781de36daa5597da6ded6213a95aa3bf93a1
preethi <preethibrgwd585@gmail.com>
2024-02-09 11:40:41 +0530

    Add project files.

---
+++ SingletonDemo/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Singleton c3 = Singleton.Instance(5,2);
+            
+           
+            Console.WriteLine("output from c3: "+c3.Add());
+            Singleton c5 = Singleton.Instance(4,5);
+          
+            Console.WriteLine("output from c5: "+c5.Add());
+            Console.WriteLine("output from c3: " + c3.Add());
+
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ SingletonDemo/Singleton.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonDemo
+{
+    public sealed class Singleton
+    {
+        private static Singleton instance=null;
+    
+
+        public int operand1 { get;  set; }
+        public int operand2 { get;  set; }
+        private Singleton(int value1, int value2)
+        {
+            operand1 = value1;
+            operand2 = value2;
+
+        }
+       public int Add()
+        {
+            return operand1 + operand2;
+        }
+        public static Singleton Instance(int value1,int value2)
+        {
+            if (instance == null)
+            {
+                instance = new Singleton(value1,value2);
+
+            }
+            return instance;
+        }
+    }
+}
